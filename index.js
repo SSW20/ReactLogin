395bcc2616550cb4abfee1ab30e09ef355d6f57a
이름 <76836826+SSW20@users.noreply.github.com>
2021-06-25 15:52:26 +0900

    비밀 설정 보호 관리

--- index.js
+++ index.js
@@ -2,16 +2,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const {User} = require('./models/User')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://anfdmlrltk11:tjddnrdl10@boiler-plate.mvkvd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify:false
 }).then(()=> console.log('MongoDB connected...'))
 .catch(err=>console.log(err))
 
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended : true}));
+//application/json
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World~~~~안녕하세요~~~~~')
+  res.send('Hello World~~~~안녕하세요~~~1111123112313231~~')
 })
+
+app.post('/register', (req, res) => {
+
+    //회원 가입할 때 필요한 정보들을 클라이언트에서 가져온 뒤 DB에 넣어준다
+
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({success : false, err})
+        return res.status(200).json({success : true})
+    })
+
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
